f4071e12003501771a8a9526f12abe0aba759bc6
Hemant Ramawat <hemant.ramawat@nyu.edu>
2023-03-27 16:59:40 -0400

    lab1: add parallel version

--- lab1/hr2378-code.c
+++ lab1/hr2378-code.c
@@ -8,19 +8,14 @@ bool DEBUG = false;
 
 void print_usage();
 void count_chars(int num,char *filename, int *solution);
+void count_chars_parallel(int N, int num, char *filename, int *solution);
+void count_chars_single_thread(int num, int step, char *list, int *solution);
 void print_solution(int *solution, int num);
 
 int main(int argc, char *argv[]){
 
-    // #pragma omp parallel
-    // {
-    //     int my_rank = omp_get_thread_num();
-    //     int thread_count = omp_get_num_threads();
-    //     printf("(%d/%d)World\n", my_rank+1, thread_count);
-    // }
-
     if(argc != 4) {
-        printf("\nIncorrect program arguments\n\n");
+        printf("\nIncorrect number of program arguments\n\n");
         print_usage();
         exit(1);
     }
@@ -28,30 +23,34 @@ int main(int argc, char *argv[]){
     int N;
     int num;
     char* filename;
-    // filename = "./tests/test_10.txt";
-    // num = 10;
 
     N = atoi(argv[1]);
     num = atoi(argv[2]);
     filename = argv[3];
 
     int solution[] = { 0, 0, 0, 0};
-    count_chars(num, filename, solution);
+    if (N == 0){
+        count_chars(num, filename, solution);
+    } else if (N>0){
+        count_chars_parallel(N, num, filename, solution);
+    } else {
+        printf("Incorrect argument N = %d. Number of threads cannot be negative.", N);
+    }
+
     print_solution(solution, num);
 }
 
 void count_chars(int num, char *filename, int *solution){
+    if(DEBUG){ printf("Serial version. Reading %d characters from %s.\n", num, filename);}
+
     FILE* file_ptr;
     char ch;
-
     file_ptr = fopen(filename, "r");
 
     if (NULL == file_ptr){
         printf("Input file %s couldn't be opened\n", filename);
         exit(1);
     }
-
-    if(DEBUG){ printf("Reading file %s for %d characters\n", filename, num);}
 
     for(int i = 0; i < num; i++){
         ch = fgetc(file_ptr);
@@ -72,10 +71,84 @@ void count_chars(int num, char *filename, int *solution){
             exit(1);
         }
     }
-
     fclose(file_ptr);
-    
     return;
+}
+
+void count_chars_parallel(int N, int num,char *filename, int *solution){
+    if(DEBUG){ printf("Paralle version. Reading %d characters from %s using %d thread(s).\n", num, filename, N);}
+
+    FILE* file_ptr;
+    file_ptr = fopen(filename, "r");
+    char *list = malloc(N);
+    char ch;
+    int step = 1 + ((num - 1) /N);
+
+    if (NULL == file_ptr){
+        printf("Input file %s couldn't be opened\n", filename);
+        exit(1);
+    }
+
+    for(int i = 0; i < num; i++){
+        ch = fgetc(file_ptr);
+        if(ch == EOF){
+            break;
+        } else {
+            list[i] = ch;
+        }
+    }
+
+    #pragma omp parallel num_threads(N)
+    {
+        count_chars_single_thread(num, step, list, solution);
+    }
+
+    free(list);
+    fclose(file_ptr);
+}
+
+void count_chars_single_thread(int num, int step, char *list, int *solution){
+    char ch;
+    int local_solution[] = { 0, 0, 0, 0};
+    int my_rank = omp_get_thread_num();
+    int start = my_rank * step;
+    int end = start + step;
+    if (end > num){
+        end = num;
+    }
+
+    if(DEBUG){
+        printf("Thread(%d): num %d step %d start %d end %d\n", my_rank, num, step, start, end);
+    }
+
+    for (int i = start; i < end; i++){
+        ch = list[i];
+        if(ch == EOF){
+            break;
+        }
+        if(DEBUG){ printf("Thread(%d): %c\n", my_rank, ch);}
+        if(ch == 'a'){
+            local_solution[0]++;
+        } else if(ch == 'b'){
+            local_solution[1]++;
+        } else if(ch == 'c'){
+            local_solution[2]++;
+        } else if(ch == 'd'){
+            local_solution[3]++;
+        } else {
+            printf("Unexpected character %c\n", ch);
+            exit(1);
+        }
+    }
+
+    #pragma omp critical
+    {
+        solution[0] += local_solution[0];
+        solution[1] += local_solution[1];
+        solution[2] += local_solution[2];
+        solution[3] += local_solution[3];
+    }
+    
 }
 
 void print_solution(int *solution, int num){
